9627b2f1b75e6853bf0a6b9b351a98637fc108df
Selaelo1 <selaelolanga10@gmail.com>
2024-10-31 10:25:53 +0200

    Create Categories.tsx

---
+++ src/components/Categories.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { CATEGORIES } from "../data/CategoryData";
+
+export default function Categories() {
+  return (
+    <div className="max-w-7xl mx-auto px-4 mb-16">
+      <div className="flex justify-between items-center space-x-8">
+        {CATEGORIES.map((category) => (
+          <Link
+            key={category.id}
+            to={`/category/${category.id}`}
+            className="flex-1 bg-gray-50 rounded-xl p-6 text-center hover:bg-gray-100 transition-colors group"
+          >
+            <category.icon className="w-8 h-8 mx-auto mb-3 group-hover:scale-110 transition-transform" />
+            <span className="font-medium">{category.name}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
