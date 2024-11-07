a9e05d9bc613db0ac671a601108bb682a368577a
Selaelo1 <selaelolanga10@gmail.com>
2024-11-07 13:47:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Stays from "./pages/Stays";
@@ -6,6 +7,7 @@ import StayDetails from "./pages/StayDetails";
 import BecomeHost from "./pages/BecomeHost";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 import CategoryPage from "./pages/CategoryPage";
 import Events from "./pages/Events";
 import Cars from "./pages/Cars";
@@ -15,27 +17,30 @@ import DestinationStays from "./pages/DestinationStays";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white">
-        <Navbar />
-        <main className="pt-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/stays" element={<Stays />} />
-            <Route path="/stays/:id" element={<StayDetails />} />
-            <Route path="/become-host" element={<BecomeHost />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/category/:id" element={<CategoryPage />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/cars" element={<Cars />} />
-            <Route path="/flights" element={<Flights />} />
-            <Route path="/guides" element={<Guides />} />
-            <Route path="/destination/:city" element={<DestinationStays />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-white">
+          <Navbar />
+          <main className="pt-20">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/stays" element={<Stays />} />
+              <Route path="/stays/:id" element={<StayDetails />} />
+              <Route path="/become-host" element={<BecomeHost />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/category/:id" element={<CategoryPage />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/cars" element={<Cars />} />
+              <Route path="/flights" element={<Flights />} />
+              <Route path="/guides" element={<Guides />} />
+              <Route path="/destination/:city" element={<DestinationStays />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
